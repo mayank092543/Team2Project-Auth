cd2ef201d7f32f881016b1fb4fa7925697c13ff6
user <user@this.com>
2022-08-30 20:30:59 -0400

    Updated

---
+++ src/main/java/com/bah/mcc/security/ChrisMayankAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bah.mcc.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChrisMayankAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChrisMayankAuthServiceApplication.class, args);
+	}
+
+}
